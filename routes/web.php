655c569d7502c664b7d74fe50a4731ecf96e13d9
Lucas Morales <lucasvmorales@protonmail.com>
2024-07-16 17:46:50 -0300

    feat: defining routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:api')->group(function () {
+    Route::get('items', [ItemController::class, 'index']);
+    Route::get('items/{id}', [ItemController::class, 'show']);
 });
