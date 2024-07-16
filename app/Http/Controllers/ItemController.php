8bf415a5b447979dc10c123f5b8cca8ca0753721
Lucas Morales <lucasvmorales@protonmail.com>
2024-07-16 17:49:22 -0300

    feat: adding Swagger and using it on Item Controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,14 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 
+/**
+ * @OA\Info(title="Item API", version="1.0")
+ */
 class ItemController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/items",
+     *     summary="List all items",
+     *     @OA\Response(response="200", description="An Items list"),
+     * )
+     */
     public function index()
     {
         $items = Item::all();
         return response()->json($items);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/items/{id}",
+     *     summary="Search an item by ID",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="200", description="Item details"),
+     *     @OA\Response(response="404", description="Item not found"),
+     * )
+     */
     public function show($id)
     {
         $item = Item::find($id);
@@ -23,3 +47,4 @@ class ItemController extends Controller
         }
     }
 }
+
